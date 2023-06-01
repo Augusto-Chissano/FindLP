0270dfeb0134ea3f02e2c392baf0c72cd110a972
Augusto <shuaiksakur@gmail.com>
2023-06-01 04:00:51 +0200

    Projecto reestruturado

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,43 +1,78 @@
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-const URL = "http://localhost:3333/login";
-const btnLogin = document.querySelector(".login-btn");
+const baseURL = "http://localhost:3333"
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const btnLogin = document.querySelector(".login-btn")
 
-const logar = async (email, password) => {
+const login = async (email, password) => {
 
-    const response = await fetch(URL, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: email, password: password })
-    });
+    try {
+        const user = { email, password }
+        const response = await fetch(`${baseURL}/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
 
-    const { user, token } = await response.json();
-
-    if (user) {
-        localStorage.setItem('token', token)
-        localStorage.setItem('user', JSON.stringify(user))
-        
-        return window.location.href = 'home.html'
-    } else {
-        emailInput.style.borderColor = "red";
-        passwordInput.style.borderColor = "red";
+        if (response.ok) {
+            const data = await response.json();
+            const { user, token } = data
+            return { user, token }
+        } else {
+            const data = await response.json();
+            return alert(data.error)
+        }
+    } catch (error) {
+        console.error(`Ocorreu um erro: ${error}`)
+        throw error
     }
 }
 
-btnLogin.addEventListener("click", (event) => {
+const handleLogin = async () => {
+    const email = emailInput.value
+    const password = passwordInput.value
 
-    event.preventDefault();
-    logar(emailInput.value, passwordInput.value);
-});
+    try {
+        const { user, token } = await login(email, password)
+        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('token', token)
+        //Redirecionamento para a tela principal
+        return window.location.href = 'home.html'
 
-emailInput.addEventListener("click", () => {
-    emailInput.style.borderColor = "#ccc";
-});
+    } catch (error) {
+        console.error('Erro durante o login:', error)
+       // emailInput.style.outlineColor = 'tomato'
+       // passwordInput.style.outlineColor = 'tomato'
+    }
+}
 
-passwordInput.addEventListener("click", () => {
-    passwordInput.style.borderColor = "#ccc";
-});
+
+
+btnLogin.addEventListener("click", async (event) => {
+
+    event.preventDefault()
+    await handleLogin()
+})
+
+emailInput.addEventListener('click', () => {
+    emailInput.style.outlineColor = '#ccc'
+})
+
+passwordInput.addEventListener('click', () => {
+    passwordInput.style.outlineColor = '#ccc'
+})
+
+emailInput.addEventListener('blur', () => {
+    if (!isEmail(emailInput.value)) {
+        emailInput.style.outlineColor = 'tomato'
+    }
+})
+
+function isEmail(value) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(value)
+}
+
 
 
